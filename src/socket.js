4c510df1566080817e7020df1f8971bdce961af5
William Rodríguez <williamrodriguez@Mac-mini-de-William.local>
2023-03-28 14:38:34 +0200

    URL SOCKET socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : process.env.REACT_APP_URL_API;
+const URL = process.env.NODE_ENV === 'production' ? undefined : process.env.URL_SOCKET;
 
 export const socket = io(URL);
